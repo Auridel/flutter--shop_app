32c150faa9330ef8b03235de3411ca1cf36520dc
Oleg Efimov <efimov.oleg.87@gmail.com>
2021-07-13 22:33:31 +0300

    global filters

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum EFilterOptions {
+  Favorites,
+  All,
+}
 
+class ProductOverviewScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('My shop'),
-        ),
-        body: ProductsGrid(),
-      );
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: EFilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: EFilterOptions.All,
+              ),
+            ],
+            icon: Icon(Icons.more_vert),
+            onSelected: (EFilterOptions selectedValue) {
+              if(selectedValue == EFilterOptions.Favorites) {
+                productsData.showFavoritesOnly();
+              } else {
+                productsData.showAll();
+              }
+            },
+          )
+        ],
+      ),
+      body: ProductsGrid(),
+    );
+  }
 }
